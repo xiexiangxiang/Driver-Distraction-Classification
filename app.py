3dbf912bf631de3cc26a2e7dd5eca6f1fcdb2d35
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-01-10 21:06:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,28 @@ import PIL
 import matplotlib.image as mpimg
 import time
 
+# funtions
+def predict_img(model_export_url, img, display_img):
+  st.image(display_img, use_column_width=True)
+  with st.spinner('Loading...'):
+        time.sleep(1)
+  # model loading & predicting
+  urllib.request.urlretrieve(model_export_url, "model.pkl")
+  model = load_learner(Path("."), "model.pkl")
+  pred, pred_idx, probs = model.predict(img)
+  st.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
+  
+def model_options():
+  model_option = st.radio('', ['Vgg16', 'Vgg19', 'ResNet18', 'ResNet34'])
+  if model_option == 'Vgg16':
+      predict_img(Vgg16_export_url, img, display_img)
+  elif model_option == 'Vgg19':
+      predict_img(Vgg19_export_url, img, display_img)
+  elif model_option == 'ResNet18':
+      predict_img(ResNet18_export_url, img, display_img)
+  elif model_option == 'ResNet34':
+      predict_img(ResNet34_export_url, img, display_img)
+ 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Driver Distraction Classification")
 st.write('''
@@ -49,24 +71,3 @@ else:
       display_img = st.image(Uploaded, caption='Uploaded picture', use_column_width=True)
       # different model prediction
       model_options()
-
-def predict_img(model_export_url, img, display_img):
-  st.image(display_img, use_column_width=True)
-  with st.spinner('Loading...'):
-        time.sleep(1)
-  # model loading & predicting
-  urllib.request.urlretrieve(model_export_url, "model.pkl")
-  model = load_learner(Path("."), "model.pkl")
-  pred, pred_idx, probs = model.predict(img)
-  st.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
-  
-def model_options():
-  model_option = st.radio('', ['Vgg16', 'Vgg19', 'ResNet18', 'ResNet34'])
-  if model_option == 'Vgg16':
-      predict_img(Vgg16_export_url, img, display_img)
-  elif model_option == 'Vgg19':
-      predict_img(Vgg19_export_url, img, display_img)
-  elif model_option == 'ResNet18':
-      predict_img(ResNet18_export_url, img, display_img)
-  elif model_option == 'ResNet34':
-      predict_img(ResNet34_export_url, img, display_img)
