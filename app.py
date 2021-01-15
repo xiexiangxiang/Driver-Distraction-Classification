711a49e148ba6cc03a72657bf98a4ddce3173767
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-01-15 21:37:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predict_img(model_export_url, img, display_img):
 def plot_interp(data, model_weight_url, model_arch):
   if st.button("Show Graphs"):
     gdown.download(model_weight_url, 'model.pth', quiet=False)
-    model = cnn_learner(data, model_arch, metrics=accuracy).load("model.pth") # path => 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth'
+    model = cnn_learner(data, model_arch, metrics=accuracy).load("model") # path => 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth'
     interp = ClassificationInterpretation.from_learner(model)
     ## 1. Test accuracy
     preds, y = model.get_preds(ds_type=DatasetType.Valid)
@@ -56,7 +56,8 @@ def model_options(predict=False, show_performance=False):
     elif model_option == 'ResNet34':
       plot_interp(data, ResNet34_weight_url, models.resnet34)
 
-@st.cache(ttl=3600, max_entries=10)
+#@st.cache(ttl=3600, max_entries=10)
+@st.cache
 def get_data(Dataset_Zip_url):
   gdown.download(Dataset_Zip_url, 'data.zip', quiet=False) # Load dataset zip
   zf.ZipFile('data.zip').extractall() # After extract => Data Folder 'AUC_Distracted_Driver_Dataset' obtained
