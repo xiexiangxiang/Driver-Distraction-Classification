1c41b4642ae759545d629e32da5fe7ef09fbe8f8
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-01-16 18:19:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from fastai.metrics import *
 import urllib.request # get model.pkl
 import PIL
 import matplotlib.image as mpimg
-import gdown # get data.zip & model.pth ## Due to *Large File Size*
-import zipfile as zf # extract data.zip
 import time
 
 ## Funtions
@@ -20,20 +18,6 @@ def predict_img(model_export_url, img, display_img):
     model = load_learner(Path("."), "model.pkl")
     pred, pred_idx, probs = model.predict(img)
     st.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
-
-def plot_interp(learn_model):
-  if st.button("Show Graphs"):
-    #learn_model = load_model_weight(data, model_weight_url, model_arch)
-    interp = ClassificationInterpretation.from_learner(learn_model)
-    ## 1. Test accuracy
-    preds, y = learn_model.get_preds(ds_type=DatasetType.Valid)
-    st.write('Test accuracy = ', accuracy(preds, y).item())
-    '''
-    ## 2. Plot confusion matrix
-    interp.plot_confusion_matrix(figsize=(11,11), dpi=60)
-    ## 3. Visualise most wrongly predicted images
-    interp.plot_top_losses(9, figsize=(15,11))
-    '''
   
 def model_options(predict=False, show_performance=False):
   model_option = st.radio('', ['Vgg16', 'Vgg19', 'ResNet18', 'ResNet34'])
@@ -46,20 +30,9 @@ def model_options(predict=False, show_performance=False):
       predict_img(ResNet18_export_url, img, display_img)
     elif model_option == 'ResNet34':
       predict_img(ResNet34_export_url, img, display_img)
-  
   elif show_performance == True:
-    if model_option == 'Vgg16':
-      model = load_model_weight(data, Vgg16_weight_url, models.vgg16_bn)
-      plot_interp(model)
-    elif model_option == 'Vgg19':
-      model = load_model_weight(data, Vgg19_weight_url, models.vgg19_bn)
-      plot_interp(model)
-    elif model_option == 'ResNet18':
-      model = load_model_weight(data, ResNet18_weight_url, models.resnet18)
-      plot_interp(model)
-    elif model_option == 'ResNet34':
-      model = load_model_weight(ResNet34_weight_url, models.resnet34)
-      plot_interp(model)
+    link = '[Google Colab](https://drive.google.com/file/d/1NfMrSk25HBwjGsLYctjPFLojg5UWDMyo/view?usp=sharing)'
+    st.markdown(link, unsafe_allow_html=True)
 
 # ---
 # taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
@@ -87,48 +60,16 @@ def save_response_content(response, destination):
         for chunk in response.iter_content(CHUNK_SIZE):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
-# ---
-      
-@st.cache(ttl=3600, max_entries=10, allow_output_mutation=True)
-def get_data(Dataset_Zip_url):
-  gdown.download(Dataset_Zip_url, 'data.zip', quiet=False) # Load dataset zip
-  zf.ZipFile('data.zip').extractall() # After extract => Data Folder 'AUC_Distracted_Driver_Dataset' obtained
-  path = 'AUC_Distracted_Driver_Dataset/Camera1/'
-  data = ImageDataBunch.from_folder(path, train='train', valid='test', ds_tfms=get_transforms(do_flip=False), size=(223,433), bs=32).normalize(imagenet_stats)
-  return data
-
-@st.cache(ttl=7200, max_entries=10, allow_output_mutation=True)
-def load_model_weight(dataset, model_weight_url, model_arch):
-  gdown.download(model_weight_url, 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth', quiet=False)
-  model = cnn_learner(dataset, model_arch, metrics=accuracy).load("model") # path => 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth'
-  return model
   
 # Pages
 page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Baseline Model Performance'])
 #st.set_option('deprecation.showfileUploaderEncoding', False)
-
-# Data -- gdown
-DataZip_url = 'https://drive.google.com/uc?id=1Hy9tdBjd7qOucIgIiMFYu9mb0_9ng6xx' #add temp empty file named "models"
 
 # Model Export URL -- urllib.request
 Vgg16_export_url = "https://drive.google.com/uc?export=download&id=12zOXR8qUdnjsc4JvwMHfj4Pq7_fS1Hsg"
 Vgg19_export_url = "https://drive.google.com/uc?export=download&id=1hVShpb9k2o3hLqY2x4ShQ_jR5Uqb1Bjo"
 ResNet18_export_url = "https://drive.google.com/uc?export=download&id=1sNmlieI8bJB6yGyOTgj-r5mGhKBbrltd"
 ResNet34_export_url = "https://drive.google.com/uc?export=download&id=1r8ohh3cLc1dKP7bLW_OZMfeBDAj0Ugxh"
-
-# Model Weight URL -- gdown
-Vgg16_weight_url = "https://drive.google.com/uc?id=1BDFbhKcteZ95rBzhkpRjq1Cxy3f4PMXf"
-Vgg19_weight_url = "https://drive.google.com/uc?id=1-NpZYlfUmnKCJihA2BRt7Ws38me_pkfr"
-ResNet18_weight_url = "https://drive.google.com/uc?id=1zNjarWxGld7uF4iBze0ZQQx_5SfRejvG"
-ResNet34_weight_url = "https://drive.google.com/uc?id=1ZItSPxQ6k_oaR-t-6H6fE3ubJZFj3Z9E"
-
-download_file_from_google_drive("1Hy9tdBjd7qOucIgIiMFYu9mb0_9ng6xx", "data.zip")
-zf.ZipFile('data.zip').extractall() # After extract => Data Folder 'AUC_Distracted_Driver_Dataset' obtained
-path = 'AUC_Distracted_Driver_Dataset/Camera1/'
-data = ImageDataBunch.from_folder(path, train='train', valid='test', ds_tfms=get_transforms(do_flip=False), size=(223,433), bs=32).normalize(imagenet_stats)
-  
-download_file_from_google_drive("1BDFbhKcteZ95rBzhkpRjq1Cxy3f4PMXf", "AUC_Distracted_Driver_Dataset/Camera1/models/model.pth")
-model = cnn_learner(data, models.vgg16_bn, metrics=accuracy).load("/content/model_weight") # path => 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth'
 
 ## Page - Baseline Model Prediction
 if page == 'Baseline Model Prediction':
@@ -172,38 +113,9 @@ elif page == 'Baseline Model Performance':
   st.title("Driver Distraction Classification")
   st.write('''
   ---
-  ## ** Baseline Model Performance**
+  ## ** Baseline Model Performance [Google Colab] **
    **Test Accuracy,
    Confusion Matrix,
    Most Wronly Predicted Classes**
    ''')
-  # get Data
-  '''
-  data = get_data(DataZip_url)
-  st.write("data classes", len(data.classes))
-  
-  # load model weight
-  model = load_model_weight(data, Vgg16_weight_url, models.vgg16_bn)
-  
-  if st.button("show"):
-    preds, y = model.get_preds(ds_type=DatasetType.Valid)
-    st.write('Test accuracy = ', accuracy(preds, y).item())
-  '''
-  # different model performance
-  #model_options(show_performance=True)
-  '''
-  download_file_from_google_drive("1Hy9tdBjd7qOucIgIiMFYu9mb0_9ng6xx", "data.zip")
-  zf.ZipFile('data.zip').extractall() # After extract => Data Folder 'AUC_Distracted_Driver_Dataset' obtained
-  path = 'AUC_Distracted_Driver_Dataset/Camera1/'
-  data = ImageDataBunch.from_folder(path, train='train', valid='test', ds_tfms=get_transforms(do_flip=False), size=(223,433), bs=32).normalize(imagenet_stats)
-  st.write("data classes: ", len(data.classes))
-  
-  download_file_from_google_drive("1BDFbhKcteZ95rBzhkpRjq1Cxy3f4PMXf", "AUC_Distracted_Driver_Dataset/Camera1/models/model.pth")
-  st.write("model weight downloaded")
-  if st.button("load model weight"):
-    model = cnn_learner(data, models.vgg16_bn, metrics=accuracy).load("/content/model_weight") # path => 'AUC_Distracted_Driver_Dataset/Camera1/models/model.pth'
-  st.write("model weight loaded")
-  '''
-  if st.button("Find accuracy"):
-    preds, y = model.get_preds(ds_type=DatasetType.Valid)
-    st.write('Test accuracy = ', accuracy(preds, y).item())
+  model_options(show_performance=True)
