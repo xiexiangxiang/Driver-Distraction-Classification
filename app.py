777beca6d5535a1541dd9be99d85473202d25e6f
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-02-26 17:33:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ import time
 ## Funtions
 def predict_img(model_export_url, img, display_img):
   # Display Image
-  st.image(display_img, use_column_width=True)
-  with st.spinner('Loading...'):
-        time.sleep(2)
+  col2.image(display_img, use_column_width=True)
+  with col2.spinner('Loading...'):
+        time.sleep(3)
   # Model Loading & Prediction
-  if st.button("Analyse"):
+  if col2.button("Analyse"):
     urllib.request.urlretrieve(model_export_url, "model.pkl")
     model = load_learner(Path("."), "model.pkl")
     pred, pred_idx, probs = model.predict(img)
-    st.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
-    st.write(probs,'%')
+    col2.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
+    col2.write(probs,'%')
   
-def model_options(predict=False, show_performance=False):
-  model_option = st.radio('', ['Vgg16', 'Vgg19', 'ResNet18', 'ResNet34'])
+def model_options(predict=False):
+  model_option = col1.radio('', ['Vgg16', 'Vgg19', 'ResNet18', 'ResNet34'])
   if predict == True:
     if model_option == 'Vgg16':
       predict_img(Vgg16_export_url, img, display_img)
@@ -31,12 +31,9 @@ def model_options(predict=False, show_performance=False):
       predict_img(ResNet18_export_url, img, display_img)
     elif model_option == 'ResNet34':
       predict_img(ResNet34_export_url, img, display_img)
-  elif show_performance == True:
-    link = '[Google Colab](https://colab.research.google.com/drive/1YWqFjd_2PXyu70D-SHcwaKbZaOs5BNU-?usp=sharing)'
-    st.markdown(link, unsafe_allow_html=True)
 
 # Pages
-page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Baseline Model Performance'])
+page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Ensemble Model Prediction'])
 #st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Model Export URL -- urllib.request
@@ -51,11 +48,13 @@ ResNet18_b_export_url = "https://drive.google.com/uc?export=download&id=1QrZ8XMt
 ResNet34_b_export_url = "https://drive.google.com/uc?export=download&id=1UoXGwiWn1YV9hnJhLTpSe1ug_yka6u6t"
 
 ## Page - Baseline Model Prediction
+
 if page == 'Baseline Model Prediction':
-  st.title("Driver Distraction Classification")
+  link = '[Google Colab](https://colab.research.google.com/drive/1YWqFjd_2PXyu70D-SHcwaKbZaOs5BNU-?usp=sharing)'
+  st.title("Baseline CNNs Driver Distraction Classification", st.markdown(link, unsafe_allow_html=True))
   st.write('''
   ---
-  ## ** Baseline Model - Classify 10 types of distracted driver behaviour**
+  ## ** Classify 10 types of distracted driver behaviour**
    **c0: Safe Driving, 
    c1: Texting - Right, 
    c2: Talking on the Phone - Right, 
@@ -68,33 +67,28 @@ if page == 'Baseline Model Prediction':
    c9: Talking to Passenger**
    '''
           )
-  # Try test image / Upload image
-  option = st.radio('', ['Try a test image', 'Upload image'])
+  # create 2 columns structure
+  col1,col2 = st.beta_columns(1,2) # 2nd column is 2 times of 1st column
+
+  # 1st column - Try test image / Upload image
+  option = col1.radio('', ['Try a test image', 'Upload image'])
   if option == 'Try a test image':
     test_imgs = os.listdir('test-image/')
-    test_img = st.selectbox('Please select a test image:', test_imgs)
+    test_img = col1.selectbox('Please select a test image:', test_imgs)
     file_path = 'test-image/' + test_img
     img = open_image(file_path)
     display_img = mpimg.imread(file_path)
     # different model prediction
     model_options(predict=True)
   else:
-    st.write('''### Please upload a picture for one of these distracted driver behaviour''')
-    Uploaded = st.file_uploader('', type=['png','jpg','jpeg'])
+  st.write('''### Please upload a picture for one of these distracted driver behaviour''')
+    Uploaded = col1.file_uploader('', type=['png','jpg','jpeg'])
     if Uploaded is not None:
       img = open_image(Uploaded)
       display_img = Uploaded
       # different model prediction
       model_options(predict=True)
-  
-## Page - Baseline Model Performance
-elif page == 'Baseline Model Performance':
-  st.title("Driver Distraction Classification")
-  st.write('''
-  ---
-  ## ** Baseline Model Performance [Google Colab] **
-   **Test Accuracy,
-   Confusion Matrix,
-   Most Wronly Predicted Classes**
-   ''')
-  model_options(show_performance=True)
+
+## Page - Ensemble Model Prediction
+elif page == 'Ensemble Model Prediction':
+  st.write("...")
