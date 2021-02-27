53a1c3cf7fc1fa72358f2ef568d9ab2e48f39f40
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-02-27 15:02:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,40 +7,71 @@ import matplotlib.image as mpimg
 import time
 
 ## Funtions
-def predict_img(col2, model_export_url, img, display_img):
-  # Display Image
-  col2.image(display_img, use_column_width=True)
+def predict_img(col2, model_export_url, img):
   # Model Loading & Prediction
   if col2.button("Analyse"):
     with st.spinner('Loading...'):
-      time.sleep(5)
+      time.sleep(3)
     urllib.request.urlretrieve(model_export_url, "model.pkl")
     model = load_learner(Path("."), "model.pkl")
     pred, pred_idx, probs = model.predict(img)
     st.write("Model Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
     st.write(probs,'%')
-  
-def base_model_options(col1, col2, img, display_img, predict=False):
+ 
+def base_model_options(col1, col2, img, predict=False):
   model_option = col1.radio('Choose a model:', ['Vgg16', 'Vgg16_b', 'Vgg19','Vgg19_b', 'ResNet18', 'ResNet18_b', 'ResNet34', 'ResNet34_b'])
   if predict == True:
     if model_option == 'Vgg16':
-      predict_img(col2, Vgg16_export_url, img, display_img)
+      predict_img(col2, Vgg16_export_url, img)
     elif model_option == 'Vgg16_b':
-      predict_img(col2, Vgg16_b_export_url, img, display_img)
+      predict_img(col2, Vgg16_b_export_url, img)
     elif model_option == 'Vgg19':
-      predict_img(col2, Vgg19_export_url, img, display_img)
+      predict_img(col2, Vgg19_export_url, img)
     elif model_option == 'Vgg19_b':
-      predict_img(col2, Vgg19_b_export_url, img, display_img)
+      predict_img(col2, Vgg19_b_export_url, img)
     elif model_option == 'ResNet18':
-      predict_img(col2, ResNet18_export_url, img, display_img)
+      predict_img(col2, ResNet18_export_url, img)
     elif model_option == 'ResNet18_b':
-      predict_img(col2, ResNet18_b_export_url, img, display_img)
+      predict_img(col2, ResNet18_b_export_url, img)
     elif model_option == 'ResNet34':
-      predict_img(col2, ResNet34_export_url, img, display_img)
+      predict_img(col2, ResNet34_export_url, img)
     elif model_option == 'ResNet34_b':
-      predict_img(col2, ResNet34_b_export_url, img, display_img)
+      predict_img(col2, ResNet34_b_export_url, img)
 
-def input_image(try_test_image=False, upload_image=False):
+def ensemble_model_options(col1, col2, img):
+  model_option = col1.radio('Choose a model:', ['Vgg16 + Vgg19 + ResNet18_b', 
+                                                'Vgg16_b + Vgg19_b + ResNet18_b', 
+                                                'Vgg16 + Vgg19 + ResNet18_b + ResNet34_b',
+                                                'Vgg16 + Vgg19', 
+                                                'Vgg19 + ResNet18_b'])
+  urllib.request.urlretrieve(Vgg16_export_url, "Vgg16.pkl")
+  urllib.request.urlretrieve(Vgg16_b_export_url, "Vgg16_b.pkl")
+  urllib.request.urlretrieve(Vgg19_export_url, "Vgg19.pkl")
+  urllib.request.urlretrieve(Vgg19_b_export_url, "Vgg19_b.pkl")
+  urllib.request.urlretrieve(ResNet18_b_export_url, "ResNet18_b.pkl")
+  urllib.request.urlretrieve(ResNet34_b_export_url, "ResNet34_b.pkl")
+  if model_option == 'Vgg16 + Vgg19 + ResNet18_b':
+    if col2.button("Analyse"):
+      with st.spinner('Loading...'):
+        time.sleep(3)
+      Vgg16 = load_learner(Path("."), "Vgg16.pkl")
+      Vgg19 = load_learner(Path("."), "Vgg19.pkl")
+      ResNet18_b = load_learner(Path("."), "ResNet18_b.pkl")
+      _, __, probs1 = Vgg16.predict(img)
+      _, __, probs2 = Vgg19.predict(img)
+      _, __, probs3 = ResNet18_b.predict(img)
+      avg_probs = (probs1 + probs2 + probs3)/3
+      ensemble_pred, ensemble_prob = torch.max(avg_probs, 0)
+      st.write("Model Prediction: ", ensemble_pred, "; Probability: ", ensemble_prob*100,'%')
+    elif model_option == 'Vgg16_b + Vgg19_b + ResNet18_b':
+      st.write('.')
+    elif model_option == 'Vgg16 + Vgg19 + ResNet18_b + ResNet34_b':
+      st.write('.')
+    elif model_option == 'Vgg16 + Vgg19':
+      st.write('.')
+    elif model_option == 'Vgg19 + ResNet18_b':
+      
+def input_image(try_test_image=False, upload_image=False, base_model=False, ensemble_model=False):
   if try_test_image == True:
     # create 2 columns structure
     col1,col2 = st.beta_columns([1,2]) # 2nd column is 2 times of 1st column
@@ -49,14 +80,26 @@ def input_image(try_test_image=False, upload_image=False):
     file_path = 'test-image/' + test_img
     img = open_image(file_path)
     display_img = mpimg.imread(file_path)
-    base_model_options(col1, col2, img, display_img, predict=True)
+    # Display Image
+    col2.image(display_img, use_column_width=True)
+    if base_model==True:
+      base_model_options(col1, col2, img, predict=True)
+    elif ensemble_model==True:
+      ensemble_model_options(col1, col2, img)
+      
   elif upload_image == True:
     Uploaded = st.file_uploader('', type=['png','jpg','jpeg'])
     if Uploaded is not None:
+      # create 2 columns structure
       col1,col2 = st.beta_columns([1,2])
       img = open_image(Uploaded)
       display_img = Uploaded
-      base_model_options(col1, col2, img, display_img, predict=True)
+      # Display Image
+      col2.image(display_img, use_column_width=True) 
+      if base_model==True:
+        base_model_options(col1, col2, img, predict=True)
+      elif ensemble_model==True:
+      ensemble_model_options(col1, col2, img)
 
 # Pages
 page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Ensemble Model Prediction'])
@@ -96,9 +139,9 @@ if page == 'Baseline Model Prediction':
   # Try test image / Upload image
   option = st.radio('Choose a distrated drving image', ['Try a test image', 'Upload an image'])
   if option == 'Try a test image':
-    input_image(try_test_image=True)
+    input_image(try_test_image=True, base_model=True)
   else:
-    input_image(upload_image=True)
+    input_image(upload_image=True, base_model=True)
 
 ## 2. Page - Ensemble Model Prediction
 elif page == 'Ensemble Model Prediction':
@@ -111,6 +154,12 @@ elif page == 'Ensemble Model Prediction':
    4: Vgg16 + Vgg19, 
    5: Vgg19 + ResNet18_b, 
   ''')
+  link = '[Google Colab](https://colab.research.google.com/drive/1YWqFjd_2PXyu70D-SHcwaKbZaOs5BNU-?usp=sharing)'
   st.markdown(link, unsafe_allow_html=True)
   st.write('''---''')
+  option = st.radio('Choose a distrated drving image', ['Try a test image', 'Upload an image'])
+  if option == 'Try a test image':
+    input_image(try_test_image=True, ensemble_model=True)
+  else:
+    input_image(upload_image=True, ensemble_model=True)
   
