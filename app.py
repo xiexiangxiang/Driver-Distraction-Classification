c60bd57fea4fdb34fc44e6643d11402976cd6865
xiexiangxiang <59919545+xiexiangxiang@users.noreply.github.com>
2021-02-28 15:24:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,6 @@ def ensemble_model_options(col1, col2, img):
       ensemble_prob, ensemble_idx = torch.max(avg_probs, 0)
       st.write("Model Prediction: C", ensemble_idx.item(), "; Probability: ", ensemble_prob.item()*100,'%')
       st.write(avg_probs,'%')
-      
   elif model_option == 'Vgg16_b + Vgg19_b + ResNet18_b':
     if col2.button("Analyse"):
       with st.spinner('Loading...'):
@@ -86,7 +85,6 @@ def ensemble_model_options(col1, col2, img):
       ensemble_prob, ensemble_idx = torch.max(avg_probs, 0)
       st.write("Model Prediction: C", ensemble_idx.item(), "; Probability: ", ensemble_prob.item()*100,'%')
       st.write(avg_probs,'%')
-      
   elif model_option == 'Vgg16 + Vgg19 + ResNet18_b + ResNet34_b':
     if col2.button("Analyse"):
       with st.spinner('Loading...'):
@@ -99,7 +97,6 @@ def ensemble_model_options(col1, col2, img):
       ensemble_prob, ensemble_idx = torch.max(avg_probs, 0)
       st.write("Model Prediction: C", ensemble_idx.item(), "; Probability: ", ensemble_prob.item()*100,'%')
       st.write(avg_probs,'%')
-      
   elif model_option == 'Vgg16 + Vgg19':
     if col2.button("Analyse"):
       with st.spinner('Loading...'):
@@ -110,7 +107,6 @@ def ensemble_model_options(col1, col2, img):
       ensemble_prob, ensemble_idx = torch.max(avg_probs, 0)
       st.write("Model Prediction: C", ensemble_idx.item(), "; Probability: ", ensemble_prob.item()*100,'%')
       st.write(avg_probs,'%')
-      
   elif model_option == 'Vgg19 + ResNet18_b':
     if col2.button("Analyse"):
       with st.spinner('Loading...'):
@@ -121,7 +117,33 @@ def ensemble_model_options(col1, col2, img):
       ensemble_prob, ensemble_idx = torch.max(avg_probs, 0)
       st.write("Model Prediction: C", ensemble_idx.item(), "; Probability: ", ensemble_prob.item()*100,'%')
       st.write(avg_probs,'%')
-      
+
+## Large file download functions
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+    save_response_content(response, destination)    
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: 
+                f.write(chunk)
+
+def hybrid_model_options(col1, col2, img):
+  model_option = col1.radio('Choose a model:', ['Vgg16 + SVM', 'Vgg16 + MLP'])
+  download_file_from_google_drive("FILEID", "FILEPATH") 
+                
 def input_image(try_test_image=False, upload_image=False, base_model=False, ensemble_model=False):
   if try_test_image == True:
     # create 2 columns structure
@@ -153,7 +175,7 @@ def input_image(try_test_image=False, upload_image=False, base_model=False, ense
         ensemble_model_options(col1, col2, img)
 
 # Pages
-page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Ensemble Model Prediction'])
+page = st.sidebar.selectbox("Choose a page", ['Baseline Model Prediction', 'Ensemble Model Prediction', 'Hybrid Model Prediction'])
 #st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Model Export URL -- urllib.request
@@ -161,7 +183,6 @@ Vgg16_export_url = "https://drive.google.com/uc?export=download&id=1jiRNwi2e_kfh
 Vgg19_export_url = "https://drive.google.com/uc?export=download&id=1qFyLokQD_AmhQEiJwAjo4gF0wfBZoh2a"
 ResNet18_export_url = "https://drive.google.com/uc?export=download&id=1mQzySjxlfZdzW88UrM74pQ_duLY67k2a"
 ResNet34_export_url = "https://drive.google.com/uc?export=download&id=16vDbA-yqcRBGwk_QEcLJcixn8TBYmGLT"
-
 Vgg16_b_export_url = "https://drive.google.com/uc?export=download&id=1hmuOsHrkcKbe3doKGage7UBKgy9Xd9_Z"
 Vgg19_b_export_url = "https://drive.google.com/uc?export=download&id=1O83NNtBBZ4mH4p4kw5dPXDNPwuFoxBgn"
 ResNet18_b_export_url = "https://drive.google.com/uc?export=download&id=1QrZ8XMtqoKqAAMcAEfP14YsM1E4s5A5f"
@@ -211,3 +232,17 @@ elif page == 'Ensemble Model Prediction':
   else:
     input_image(upload_image=True, ensemble_model=True)
   
+## 3. Page - Hybrid Model Prediction
+elif page == 'Hybrid Model Prediction':
+  st.title("Hybrid Driver Distraction Classification")
+  st.write('''## ** [VGG16 + SVM] & [VGG16 + MLP] **''')
+  link1 = '[Google Colab](https://colab.research.google.com/drive/1YWqFjd_2PXyu70D-SHcwaKbZaOs5BNU-?usp=sharing)'
+  link2 = '[Github](https://github.com/xiexiangxiang/Driver-Distraction-Classification)'
+  st.markdown(link1, unsafe_allow_html=True)
+  st.markdown(link2, unsafe_allow_html=True)
+  st.write('''---''')
+  option = st.radio('Choose a distrated drving image', ['Try a test image', 'Upload an image'])
+  if option == 'Try a test image':
+    input_image(try_test_image=True, ensemble_model=True)
+  else:
+    input_image(upload_image=True, ensemble_model=True)
